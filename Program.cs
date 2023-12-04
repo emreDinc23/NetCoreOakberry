6171a32027c763446d36dc654b089704729dbfa0
emreDinc23 <emre_2350@outlook.com>
2023-12-04 11:02:35 +0300

    fix

--- Program.cs
+++ Program.cs
@@ -11,11 +11,6 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(connectionString);
 });
 var app = builder.Build();
-using var scope = app.Services.CreateScope();
-var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-dbContext.Database.EnsureDeleted();
-dbContext.Database.EnsureCreated();
-DbSeeder.SeedAll(dbContext);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
